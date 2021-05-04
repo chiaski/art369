75e941e4fdb1ab022c4b4e09b56ff747affa442a
Chia Amisola <chiamisola@gmail.com>
2021-05-04 15:44:24 -0400

    add to apocalypse

--- projects/apocalypse/script.js
+++ projects/apocalypse/script.js
@@ -10,6 +10,15 @@ function progression(clicks, truemoney) {
   if (!clicks && !truemoney) {
     return;
   }
+  
+  if(secs < 1000){
+      rate = 999999999;
+      clearTimeout(loop);
+      _log("Time is over.");
+      $("#end").fadeIn(20000).css("display", "flex");
+      $(".vfx-color").css("background", "#000000");
+    return;
+  }
 
   switch (truemoney) {
 
@@ -30,16 +39,140 @@ function progression(clicks, truemoney) {
     case 120:
       _log("Eventually, you learn to punish time itself.");
       $("button[data-unlock='2']").fadeIn("slow");
+      $(".vfx-color").css("background", "#f5f5f5");
       break;
-
+      
+    // SPEED UP TIME
+      
     case 300: 
+      _log("You learn how to rush time, slowly...");
+      $("button[data-unlock='4a']").fadeIn("slow");
+      break;
+    
+    case 305: 
+      _log("Such that every tick and bit comes a little sooner...");
+      break;
+      
+    case 310: 
+      _log("This action can be a repeated one.");
+      $(".vfx-color").css("background", "hsl(0, 0%, 92%)");
+      break;
+      
+      
+    // SLOW DOWN TIME
+      
+    case 500: 
+      _log("You also learn how to pause it...");
+      $("button[data-unlock='4b']").fadeIn("slow");
+      break;
+    
+    case 505: 
+      _log("Such that every tick is silenced...");
+      break;
+      
+    case 510: 
+      _log("This action can be a repeated one.");
+      $(".vfx-color").css("background", "hsl(0, 0%, 88%)");
+      break;
+      
+      
+    // CAPTURE TIME
+
+    case 600: 
       _log("Turning over the rickety clock, you discover an ancient dial.")
       break;
     
-    case 310:
+    case 610:
+      _log("Suddenly, you discover an affinity for progression.")
+      $("button[data-unlock='5']").fadeIn("slow");
+      break;
       
+    case 630:
+      _log("The constant moving forward of all things.");
+      $(".vfx-color").css("background", "hsl(0, 0%, 80%)");
       break;
 
+    case 650:
+      _log("Perhaps... this is a shared experience?")
+      break;
+      
+    // ---
+      
+      
+    // CAPTURE TIME
+
+    case 1000: 
+      _log("You are one with the passing of all places.");
+      $("button[data-unlock='8']").fadeIn("slow");
+      $(".vfx-color").css("background", "hsl(0, 0%, 75%)");
+      break;
+      
+      
+    case 1020: 
+      _log("You are one with the passing of all places.");
+      break;
+      
+    
+    case 1500:
+      _log("The timekeeper learns to let time pass.");
+      break;
+      
+    case 1520:
+      _log("You wait...");
+      $(".vfx-color").css("background", "hsl(0, 0%, 65%)");
+      $("button[data-unlock='8']").fadeIn("slow");
+      break;
+      
+      
+      
+    // CAPTURE RATE
+      
+    
+    case 1800:
+      _log("You become incredibly familiar with the ancient device, transforming movement itself.");
+      $(".vfx-color").css("background", "hsl(0, 0%, 45%)");
+      break;
+      
+    
+    case 1830:
+      _log("You, the timekeeper, are the equilibrium.");
+      $("button[data-unlock='6']").fadeIn("slow");
+      break;
+      
+    
+    case 3600:
+      $(".vfx-color").css("background", "hsl(0, 0%, 40%)");
+      break;
+      
+    case 4000:
+      $(".vfx-color").css("background", "hsl(0, 0%, 35%)");
+      break;
+      
+      
+    case 5000:
+      $(".vfx-color").css("background", "hsl(0, 0%, 30%)");
+      break;
+    
+    case 6000:
+      $(".vfx-color").css("background", "hsl(0, 0%, 25%)");
+      break;
+      
+      
+    case 7000:
+      $(".vfx-color").css("background", "hsl(0, 0%, 20%)");
+      break;
+      
+    case 8000:
+      $(".vfx-color").css("background", "hsl(0, 0%, 15%)");
+      break;
+      
+    case 9000:
+      $(".vfx-color").css("background", "hsl(0, 0%, 10%)");
+      break;
+      
+    case 10000:
+      $(".vfx-color").css("background", "hsl(0, 0%, 5%)");
+      break;
   }
 
 }
@@ -52,7 +185,7 @@ function progression(clicks, truemoney) {
 var money = 0; // your seconds
 var _truemoney = 0; // time that has passed, never taken away
 
-var rate = 50; // def 1 sec
+var rate = 10; // def 1 sec
 var _clicks = 0; // determines game progression
 
 var secs = seconds_since_epoch();
@@ -125,8 +258,10 @@ var ms = database.ref('clock');
 ms.get().then((snapshot) => {
 
   const data = snapshot.val();
-  console.log(data["ms"]);
+  
   secs = parseInt(data["ms"]);
+  rate = parseInt(data["rate"]);
+  
 })
 
 
@@ -156,15 +291,20 @@ function buy(item, cost) {
   let log = "Bought ";
 
   switch (item) {
-    case "rate":
+    case "rateup":
+      
+      if(rate <= 50){
+        log = "The clock is stilled. You cannot cahnge it further."
+        break;
+      }
 
       if (money >= cost) {
-        rate -= 100;
+        rate -= 50; // speed up
         money -= cost;
         log += "more time..."
         break;
       } else {
-        log = "Not enough seconds!";
+        log = "Not enough seconds...";
       }
 
       break;
@@ -173,12 +313,12 @@ function buy(item, cost) {
     case "ratedown":
 
       if (money >= cost) {
-        rate += 1000;
+        rate += 50; // slow down
         money -= cost;
-        log += "more time..."
+        log += "more time...?"
         break;
       } else {
-        log = "Not enough seconds!";
+        log = "Not enough seconds...";
       }
 
       break;
@@ -195,16 +335,35 @@ function action(what, cost) {
 
   console.log(what);
 
+  let max = 604800; // 1 week
+  let min = 86400; // 1 day
+  let r = Math.floor(Math.random() * (max - min) + min);
+  
+  
   switch (what) {
 
     case "take":
       log += "took time.";
       // the counter is in milliseconds.. so the timer is huge
 
-      let max = 604800; // 1 week
-      let min = 86400; // 1 day
+      max = 604800; // 1 week
+      min = 86400; // 1 day
 
-      let r = Math.floor(Math.random() * (max - min) + min);
+      r = Math.floor(Math.random() * (max - min) + min);
+
+      secs -= r;
+      money += r;
+
+      break;
+      
+    case "taketwo":
+      log += "waited.";
+      // the counter is in milliseconds.. so the timer is huge
+
+      max = 31556952; // 1 week
+      min = 604800; // 1 week
+
+      r = Math.floor(Math.random() * (max - min) + min);
 
       secs -= r;
       money += r;
@@ -229,12 +388,12 @@ function action(what, cost) {
 
       if (money >= cost) {
         money -= cost;
-        secs -= 1000000;
+        secs -= 3888000; // 45 days
         log += " silenced time..."
         break;
 
       } else {
-        log = "Not enough seconds!";
+        log = "Not enough seconds...";
       }
 
       break;
@@ -246,7 +405,7 @@ function action(what, cost) {
         ms.get().then((snapshot) => {
 
           // afect firebase
-          ms.set({
+          ms.update({
             ms: secs
           });
         })
@@ -258,6 +417,28 @@ function action(what, cost) {
       }
 
       break;
+      
+      
+    case "captureR":
+
+      if (money >= cost) {
+
+        ms.get().then((snapshot) => {
+
+          // afect firebase
+          ms.update({
+            rate: rate
+          });
+        })
+
+
+        log += "captured time, for all others."
+      } else {
+        log = "Try to take more time."
+      }
+
+      break;
+
 
 
 
